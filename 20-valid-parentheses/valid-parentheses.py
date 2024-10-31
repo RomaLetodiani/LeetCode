ed569ae9360fd94743107fbc9c5e6252d4e36cc7
Roman Letodiani💠 <136558219+RomaLetodiani@users.noreply.github.com>
2024-10-31 12:45:52 +0400

    Time: 4 ms (9.31%) | Memory: 16.6 MB (31.70%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = [] # O(1)
-        dic = { # O(1)
+        brackets = { # O(1)
             '(': ')',
             '{': '}',
             '[': ']'
@@ -11,9 +11,9 @@ class Solution:
             return False # O(1)
 
         for ch in s:
-            if ch in dic: # O(1)
+            if ch in brackets: # O(1)
                 stack.append(ch) # O(1)
-            elif len(stack) == 0 or dic[stack.pop()] != ch: # O(1)
+            elif len(stack) == 0 or brackets[stack.pop()] != ch: # O(1)
                 return False # O(1)
 
         return len(stack) == 0 # O(1)
