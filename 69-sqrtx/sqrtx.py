096fd1e28b078d5e96f5252ed46a2894caf0aefe
Roman Letodiani <136558219+RomaLetodiani@users.noreply.github.com>
2025-01-18 14:06:30 +0400

    Time: 5 ms (33.98%) | Memory: 17.5 MB (57.13%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -4,12 +4,17 @@ class Solution:
             return 0
 
         left, right = 1, x
+
         while left <= right:
             mid = (left + right) // 2
+            
             if mid * mid == x:
                 return mid
+            
             if mid * mid < x:
                 left = mid + 1
                 continue
+            
             right = mid - 1
+            
         return right
