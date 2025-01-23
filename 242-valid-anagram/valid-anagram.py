9cbdf76b38ae66ded8ff7e63d34d74712243fb2f
Roman Letodiani <136558219+RomaLetodiani@users.noreply.github.com>
2025-01-23 11:58:59 +0400

    Time: 13 ms (51.06%) | Memory: 18 MB (37.81%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,48 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        # # HashArray approach
+        # Time : O(N)
+        # Space : O(1)
+        counter = [0] * 26
+
+        for ch in s:
+            counter[ord(ch) - ord('a')] += 1
+
+        for ch in t:
+            counter[ord(ch) - ord('a')] -= 1
+        
+        for n in counter:
+            if n != 0:
+                return False
+
+        return True
+
+        # # inBuilt Counter approach
+        
+        # return Counter(s) == Counter(t)
+
+        # # HashTable approach
+        # sMap = {}
+        # tMap = {}
+        
+        # for ch in s:
+        #     if ch in sMap:
+        #         sMap[ch] += 1
+        #     else:
+        #         sMap[ch] = 1
+        
+        # for ch in t:
+        #     if ch in tMap:
+        #         tMap[ch] += 1
+        #     else:
+        #         tMap[ch] = 1
+
+        # for ch in tMap:
+        #     if ch not in sMap:
+        #         return False
+        #     if sMap[ch] != tMap[ch]:
+        #         return False
+
+        # return True
