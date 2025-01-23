3ca6e0c806c50e66ad3a71acafd34b895732ddd1
Roman Letodiani <136558219+RomaLetodiani@users.noreply.github.com>
2025-01-23 11:59:38 +0400

    Time: 7 ms (90.34%) | Memory: 17.8 MB (48.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,44 +5,44 @@ class Solution:
         # # HashArray approach
         # Time : O(N)
         # Space : O(1)
-        counter = [0] * 26
+        # counter = [0] * 26
 
-        for ch in s:
-            counter[ord(ch) - ord('a')] += 1
-
-        for ch in t:
-            counter[ord(ch) - ord('a')] -= 1
-        
-        for n in counter:
-            if n != 0:
-                return False
-
-        return True
-
-        # # inBuilt Counter approach
-        
-        # return Counter(s) == Counter(t)
-
-        # # HashTable approach
-        # sMap = {}
-        # tMap = {}
-        
         # for ch in s:
-        #     if ch in sMap:
-        #         sMap[ch] += 1
-        #     else:
-        #         sMap[ch] = 1
-        
-        # for ch in t:
-        #     if ch in tMap:
-        #         tMap[ch] += 1
-        #     else:
-        #         tMap[ch] = 1
+        #     counter[ord(ch) - ord('a')] += 1
 
-        # for ch in tMap:
-        #     if ch not in sMap:
-        #         return False
-        #     if sMap[ch] != tMap[ch]:
+        # for ch in t:
+        #     counter[ord(ch) - ord('a')] -= 1
+        
+        # for n in counter:
+        #     if n != 0:
         #         return False
 
         # return True
+
+        # inBuilt Counter approach
+        
+        return Counter(s) == Counter(t)
+
+        # HashTable approach
+        sMap = {}
+        tMap = {}
+        
+        for ch in s:
+            if ch in sMap:
+                sMap[ch] += 1
+            else:
+                sMap[ch] = 1
+        
+        for ch in t:
+            if ch in tMap:
+                tMap[ch] += 1
+            else:
+                tMap[ch] = 1
+
+        for ch in tMap:
+            if ch not in sMap:
+                return False
+            if sMap[ch] != tMap[ch]:
+                return False
+
+        return True
